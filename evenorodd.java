26e637bdb4fdd4b49e3c2017ed45d05e27320994
Kishorekannan15 <139007489+Kishorekannan15@users.noreply.github.com>
2023-07-12 21:12:55 +0530

    Add files via upload

---
+++ evenorodd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class evenorodd
+{
+    public static void main(String[] args)
+    {
+        int num;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the num");
+        num=sc.nextInt();
+        if(num%2==0)
+        System.out.println("even");
+        else
+        System.out.println("odd");
+        }
+}
